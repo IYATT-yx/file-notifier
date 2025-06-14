6571771d6c32406670e90bf36ee20778bfad341e
IYATT-yx <iyatt@iyatt.com>
2025-06-15 01:11:38 +0800

    first commit

---
+++ build.ps1
@@ -0,0 +1,20 @@
+$startTime = Get-Date
+
+python -m venv venv
+.\venv\Scripts\Activate.ps1
+python.exe -m pip install --upgrade pip
+pip install -r requirements.txt
+pip install nuitka==2.7.6
+
+python .\savebuildtime.py
+
+nuitka --standalone --remove-output --windows-console-mode=disable `
+--enable-plugin=tk-inter `
+--windows-icon-from-ico=.\icon.ico --include-data-file=.\icon.ico=.\ `
+--include-data-file=.\file-notifier.template=.\ `
+--output-dir=dist --output-filename=file-notifier_win_amd64 `
+.\file-notifier.py
+
+$endTime = Get-Date
+$elapsedTime = New-TimeSpan -Start $startTime -End $endTime
+Write-Output "程序构建用时：$($elapsedTime.TotalSeconds) 秒"
